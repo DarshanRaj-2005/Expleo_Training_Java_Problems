0b57c6402b5bfc3816ab03034ec291141b0cf251
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-03-10 09:56:06 +0530

    new text file is added

--- hands_on_06/Prob_001.java
+++ hands_on_06/Prob_001.java
@@ -29,6 +29,49 @@ class Person {
 	}
 }
 
+class Student extends Person {
+	String program;
+	int year;
+	double fee;
+	
+	Student(String name, String address, String program, int year, double fee) {
+		super(name,address);
+		this.program = program;
+		this.year = year;
+		this.fee = fee;
+	}
+	
+	public void setProgram(String program) {
+		
+	}
+	
+	public String getProgram() {
+		return program;
+	}
+	
+	public void setYear(int year) {
+		
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setFee(double fee) {
+		
+	}
+	
+	public double getFee() {
+		return fee;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [Person [name= " + name + ", address= "+address+" program=" +
+	    program + ", year=" + year + ", fee=" + fee + "]";
+	}
+}
+
 public class Prob_001 {
 
 	public static void main(String[] args) {
